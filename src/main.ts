c8978dd75fadf3bf4c59cca2f0b3126d1b24d943
Eugene Sadovoi <evgeni@eniks.com>
2020-06-02 16:35:34 -0700

    fix: Fixing label configuration override

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
 import * as core from '@actions/core'
 import * as github from '@actions/github'
 import * as config from './configuration'
-import {getCommits, getBranch} from './git'
+import {getCommits} from './git'
 import {getIssueIds, parseIssues} from './issues'
-import {exactLabelInfo, labelIssues, ILabelInfo, getLabelInfo} from './label'
+import {labelIssues, getLabelInfo} from './label'
 
 /**
  * Executes action.
@@ -11,28 +11,21 @@ import {exactLabelInfo, labelIssues, ILabelInfo, getLabelInfo} from './label'
 async function run(): Promise<void> {
   try {
     // Get Path to repository
-    const path = config.path
-    core.info(`Analyzing repository at ${path}`)
+    core.info(`Analyzing repository at ${config.path}`)
 
     // Name of the label
-    const branch = getBranch(path)
-    if (!branch) {
-      core.setFailed(`No repository found at path '${path}'`)
+    if (!config.branch) {
+      core.setFailed(`No repository found at path '${config.path}'`)
       return
     }
 
     // Find label
-    let label: ILabelInfo | null;
-    if (config.label) {
-      label = await exactLabelInfo(config.label)
-    } else {
-      label = await getLabelInfo(branch)
-    }
+    const label = await getLabelInfo(config.label)
 
     // Nothing to do if no label
     if (null == label) {
       core.info(
-        `Label '${config.label}' does not exist in repository '${github.context.repo.owner}/${github.context.repo.repo}', quitting...`
+        `No suitable label found in repository '${github.context.repo.owner}/${github.context.repo.repo}', quitting...`
       )
       return
     }
@@ -41,7 +34,7 @@ async function run(): Promise<void> {
     const payload = github.context.payload
 
     // Get commit messages from git
-    const commits = getCommits(path, payload.before, payload.after)
+    const commits = getCommits(config.path, payload.before, payload.after)
     const issues = parseIssues(commits)
     const issueIds = getIssueIds(issues)
     const count = await labelIssues(label, issueIds)
